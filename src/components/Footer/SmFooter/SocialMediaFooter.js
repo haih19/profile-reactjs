b61900ffd857c2b9365700835c12054e1b29469b
haih19 <haih19c@gmail.com>
2022-08-11 14:11:31 +0700

    fix bug redirect 2

--- src/components/Footer/SmFooter/SocialMediaFooter.js
+++ src/components/Footer/SmFooter/SocialMediaFooter.js
@@ -6,22 +6,22 @@ export const SocialMediaFooter = () => {
    return (
       <div className="social-media-list">
          <div className="social-media-item">
-            <Link to={'#https://www.facebook.com/hai.h19/'}>
+            <Link to={'https://www.facebook.com/hai.h19/'}>
                <i className="facebook fa-brands fa-facebook-f"></i>
             </Link>
          </div>
          <div className="social-media-item">
-            <Link to={'#https://twitter.com/haih19c'}>
+            <Link to={'https://twitter.com/haih19c'}>
                <i className="twitter fa-brands fa-twitter"></i>
             </Link>
          </div>
          <div className="social-media-item">
-            <Link to={'#https://www.instagram.com/haih19/'}>
+            <Link to={'https://www.instagram.com/haih19/'}>
                <i className="instagram fa-brands fa-instagram"></i>
             </Link>
          </div>
          <div className="social-media-item">
-            <Link to={'#https://github.com/haih19'}>
+            <Link to={'https://github.com/haih19'}>
                <i className="github fa-brands fa-github"></i>
             </Link>
          </div>
